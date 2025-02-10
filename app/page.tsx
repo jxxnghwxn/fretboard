9ab21aef13c1fca4e8b2ab4a419a36f0f03a1133
jeonghwan <jxxnghwxn@gmail.com>
2025-02-10 23:07:29 +0900

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import ControlCard from '@/app/component//FretboardController';
+import ControlCard from '@/app/component/FretboardSet';
 
-import Fretboard from '@/app/component/FretboardGenerator';
+import Fretboard from '@/app/component/FretboardGet';
 
 export default function Home() {
   return (
