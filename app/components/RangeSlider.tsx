d213b3e7617615b3d441ac27a45dd6e679dc84bf
jeonghwan <jxxnghwxn@gmail.com>
2025-02-15 23:00:34 +0900

    temp, range slider

--- app/components/RangeSlider.tsx
+++ app/components/RangeSlider.tsx
@@ -1,48 +1,56 @@
 import { useState } from 'react';
-
-// css
 import s from '@/styles/RangeSlider.module.css';
 
-export default function RangeSlider() {
+const RangeSlider = () => {
   const [minValue, setMinValue] = useState(0);
   const [maxValue, setMaxValue] = useState(24);
 
   const handleMinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Math.min(e.target.value, maxValue);
+    const value = Math.min(Number(e.target.value), maxValue - 1);
     setMinValue(value);
   };
+
   const handleMaxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Math.max(e.target.value, minValue);
+    const value = Math.max(Number(e.target.value), minValue + 1);
     setMaxValue(value);
   };
 
   return (
-    <div>
-      <input
-        type='range'
-        id='input-left'
-        className={s.input}
-        min={0}
-        max={24}
-        value={minValue}
-        onChange={handleMinChange}
-      />
-      <input
-        type='range'
-        id='input-right'
-        className={s.input}
-        min={0}
-        max={24}
-        value={maxValue}
-        onChange={handleMaxChange}
-      />
-
+    <div className={s.slider_container}>
       <div className={s.slider}>
         <div className={s.track}></div>
         <div className={s.range}></div>
         <div className={`${s.thumb} ${s.left}`}></div>
         <div className={`${s.thumb} ${s.right}`}></div>
       </div>
+
+      {/* <div className='slider-track'>
+        <div
+          className='slider-range'
+          style={{
+            left: `${minValue}%`,
+            right: `${maxValue}%`,
+          }}
+        />
+      </div> */}
+      <input
+        type='range'
+        min={0}
+        max={24}
+        value={minValue}
+        onChange={handleMinChange}
+        className='slider-input'
+      />
+      <input
+        type='range'
+        min={0}
+        max={24}
+        value={maxValue}
+        onChange={handleMaxChange}
+        className='slider-input'
+      />
     </div>
   );
-}
+};
+
+export default RangeSlider;
